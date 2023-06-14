ec92fd1d917d26910c095373534ecbe9b3d0a090
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-06-13 21:00:45 -0300

    Refatorando o form

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -22,4 +22,15 @@ export class TemplateFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  verificaValidTouched(campo: any) {
+    return !campo.valid && campo.touched;
+  }
+
+  aplicaCssErro(campo: any) {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo)
+    }
+  }
+
 }
--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -1,13 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TemplateFormComponent } from './template-form.component';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
 import { FormDebugComponent } from '../form-debug/form-debug.component';
+import { TemplateFormComponent } from './template-form.component';
 
 @NgModule({
   declarations: [
     TemplateFormComponent,
-    FormDebugComponent
+    FormDebugComponent,
+    CampoControlErroComponent
   ],
   imports: [
     CommonModule,
