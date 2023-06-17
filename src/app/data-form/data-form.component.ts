c9f97ce7d582b1f11edf153d8268973646e6dacc
Guilherme Gomes <guilherme.gomes91@outlook.com>
2023-06-17 11:56:58 -0300

    Validacoes em campos

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
   templateUrl: './data-form.component.html',
-  styleUrls: ['./data-form.component.css']
+  styleUrls: ['./data-form.component.css'],
 })
 export class DataFormComponent implements OnInit {
-
   formulario!: FormGroup;
 
-  constructor(
-    private formBuilder: FormBuilder,
-    private http: HttpClient
-  ) {}
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
 
   ngOnInit(): void {
     /*this.formulario = new FormGroup({
@@ -23,23 +24,57 @@ export class DataFormComponent implements OnInit {
     });*/
 
     this.formulario = this.formBuilder.group({
-      nome: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]],
-      email: [null, [Validators.required, Validators.email]]
+      nome: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
+      ],
+      email: [null, [Validators.required, Validators.email]],
     });
   }
 
   onSubmit() {
-    console.log(this.formulario.value);
-    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
-      .subscribe(dados => {
-        console.log(dados);
-        //reseta o form
-        this.resetar();
-      },
-      (error: any) => alert('erro'));
+    if (this.formulario.valid) {
+      console.log(this.formulario.value);
+      this.http
+        .post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+        .subscribe(
+          (dados) => {
+            console.log(dados);
+            //reseta o form
+            this.resetar();
+          },
+          (error: any) => alert('erro')
+        );
+    } else {
+      alert('Campos não preenchidos corretamente!');
+    }
   }
 
   resetar() {
     this.formulario.reset();
+  }
+
+  verificaValidTouched(campo: any): boolean | any {
+    return (
+      !this.formulario.get(campo)?.valid && this.formulario.get(campo)?.touched
+    );
+  }
+
+  verificaEmailInvalido() {
+    let campoEmail = this.formulario.get('email');
+    if (campoEmail?.errors) {
+      return campoEmail?.errors['email'] && campoEmail.touched;
+    }
+  }
+
+  aplicaCssErro(campo: any) {
+    return {
+      'has-error': this.verificaValidTouched(campo),
+      'has-feedback': this.verificaValidTouched(campo),
+    };
   }
 }
